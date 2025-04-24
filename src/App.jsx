d2570e13802d7e5af717c9e3f78f143106817cbd
sekyeong <sekoonglee@gmail.com>
2025-04-24 12:16:10 +0900

    feat: App 컴포넌트에서 기본 UI로 Home.jsx 페이지 렌더링 적용 - Home 컴포넌트를 루트로 설정하여 메인 화면 구조 연결

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import './App.css'
+import "./styles/globals.css";
+import { useEffect } from "react";
+import Home from "./pages/Home";
 
-function App() {
+export default function App() {
 
-  return (
-    <>
-      <div>sokuri</div>
-    </>
-  )
+  useEffect(() => {
+    const handleMessage = (e) => {
+      try {
+        const { action, data } = JSON.parse(e.data);
+        if (action === "RENDER_PACKING") {
+          console.log("메시지 파싱 성공", data);
+        }
+      } catch (err) {
+        console.error("메시지 파싱 실패", err);
+      }
+    };
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  }, []);
+
+  return <Home />;
 }
-
-export default App
